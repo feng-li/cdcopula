b67f4b14e33d64e831e4661e781e5261b7a7430d
Feng Li <m@feng.li>
2012-05-18 15:22:15 +0200

    Switch from 1/0 to TRUE/FALSE as variable selection indicator

--- models/initPar.R
+++ models/initPar.R
@@ -3,11 +3,11 @@
 ##' This function is only once for the first iteration. The values are updated
 ##' via MCMC scheme.
 ##' @title Setup copula model initial value.
-##' @param varSelArgs 
-##' @param betaInit 
-##' @param Mdl.X 
+##' @param varSelArgs
+##' @param betaInit
+##' @param Mdl.X
 ##' @return "list"
-##' @references 
+##' @references
 ##' @author Feng Li, Department of Statistics, Stockholm University, Sweden.
 ##' @note Created: Thu Dec 22 15:57:14 CET 2011;
 ##'       Current: Wed Jan 11 16:03:44 CET 2012.
@@ -16,7 +16,7 @@ initPar <- function(varSelArgs, betaInit, Mdl.X)
     ## The output structure.
     Mdl.betaIdx <- betaInit
     Mdl.beta <- betaInit
-    
+
     ## Loop to assign the initial values
     CompNM <- names(Mdl.X)
     for(i in CompNM)
@@ -28,41 +28,41 @@ initPar <- function(varSelArgs, betaInit, Mdl.X)
             ncolX.ij <- ncol(Mdl.X[[i]][[j]])
 ###----------------------------------------------------------------------------
 ### Initialize the variable section indicator
-###----------------------------------------------------------------------------            
+###----------------------------------------------------------------------------
             ## Initial value for variable selection indicators which can be
             ## character or vector
             varSelInitCurr <- varSelArgs[[i]][[j]][["init"]]
-            
+
             if(class(varSelInitCurr) == "character" &&
                tolower(varSelInitCurr) == "all-in")
               {
-                Mdl.betaIdx[[i]][[j]] <- array(1, c(ncolX.ij, 1))
+                Mdl.betaIdx[[i]][[j]] <- array(TRUE, c(ncolX.ij, 1))
               }
             else if(class(varSelInitCurr) == "character" &&
                     tolower(varSelInitCurr) == "all-out")
               {
-                Mdl.betaIdx[[i]][[j]] <- array(1, c(ncolX.ij, 1))
+                Mdl.betaIdx[[i]][[j]] <- array(TRUE, c(ncolX.ij, 1))
 
                 varSelCandCurr <- varSelArgs[[i]][[j]][["cand"]]
-                Mdl.betaIdx[[i]][[j]][varSelCandCurr] <- 0
+                Mdl.betaIdx[[i]][[j]][varSelCandCurr] <- FALSE
               }
             else if(class(varSelInitCurr) == "character" &&
                     tolower(varSelInitCurr) == "random")
               {
-                Mdl.betaIdx[[i]][[j]] <- array(1, c(ncolX.ij, 1))
+                Mdl.betaIdx[[i]][[j]] <- array(TRUE, c(ncolX.ij, 1))
                 varSelCandCurr <- varSelArgs[[i]][[j]][["cand"]]
                 ## Randomly pick up half in
                 betaIdxCurrSubOut <- sample(varSelCandCurr,
                                             round(length(varSelCandCurr)/2))
-                Mdl.betaIdx[[i]][[j]][betaIdxCurrSubOut] <- 0
-              }     
+                Mdl.betaIdx[[i]][[j]][betaIdxCurrSubOut] <- FALSE
+              }
             else # Do nothing, use user input
               {
-                Mdl.betaIdx[[i]][[j]] <- array(1, c(ncolX.ij, 1))
-                Mdl.betaIdx[[i]][[j]][varSelInitCurr] <- 1
+                Mdl.betaIdx[[i]][[j]] <- array(TRUE, c(ncolX.ij, 1))
+                Mdl.betaIdx[[i]][[j]][varSelInitCurr] <- TRUE
 
               }
-            
+
 ###----------------------------------------------------------------------------
 ### Initial value for coefficients
 ###----------------------------------------------------------------------------
@@ -72,19 +72,19 @@ initPar <- function(varSelArgs, betaInit, Mdl.X)
               {
                 betaInitCurr = runif(ncolX.ij, -1, 1)
                 Mdl.beta[[i]][[j]] <- array(betaInitCurr, c(ncolX.ij, 1))
-                Mdl.beta[[i]][[j]][Mdl.betaIdx[[i]][[j]] == 0] <- 0
+                Mdl.beta[[i]][[j]][!Mdl.betaIdx[[i]][[j]]] <- 0
                 ## let beta  =  0 for nu-selected variables NOTE: is this
                 ## needed? -- YES.
               }
             else # Do nothing and use user input
               {
                 Mdl.beta[[i]][[j]] <- array(betaInitCurr, c(ncolX.ij, 1))
-              }  
+              }
 
           }
       }
-    
-    
+
+
     out <- list(Mdl.beta = Mdl.beta,
                 Mdl.betaIdx = Mdl.betaIdx)
     return(out)
