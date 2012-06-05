9c9c17ecc70361aab555190ec2022f8d6778300c
Feng Li <m@feng.li>
2012-06-05 11:09:55 +0200

    Enhanced parSwap allowing parUpdate plugin

--- models/parSwap.R
+++ models/parSwap.R
@@ -1,40 +1,41 @@
-parSwap <- function(betaInput, Mdl.betaIdx = NA, parUpdate = NA)
+parSwap <- function(betaInput, Mdl.beta = NA, Mdl.betaIdx = NA, parUpdate = NA)
   {
     ## Convert betaVec to Mdl.beta
     if(is.vector(betaInput))
       {
         ## Convert the parameter vector into the model structure
-        Mdl.beta <- Mdl.betaIdx # reserve the structure
         Idx0 <- NA # the initial parameter index
         Idx1 <- 0
         for(CompCurr in names(Mdl.betaIdx))
           {
             for(parCurr in names(Mdl.betaIdx[[CompCurr]]))
               {
-                betaIdxCurr <- Mdl.betaIdx[[CompCurr]][[parCurr]]
-                betaLen <- length(betaIdxCurr)
-                betaLenNoZero <-sum(betaIdxCurr)
+                if(parUpdate[[CompCurr]][[parCurr]])
+                  {
+                    betaIdxCurr <- Mdl.betaIdx[[CompCurr]][[parCurr]]
+                    betaLen <- length(betaIdxCurr)
+                    betaLenNoZero <-sum(betaIdxCurr)
 
-                Idx0 <- Idx1+1
-                Idx1 <- Idx0 + betaLenNoZero -1
-                IdxCurr <-Idx0:Idx1
+                    Idx0 <- Idx1+1
+                    Idx1 <- Idx0 + betaLenNoZero -1
+                    IdxCurr <-Idx0:Idx1
 
-                betaCurr <- matrix(0, betaLen, 1)
-                betaCurr[betaIdxCurr] <- betaInput[IdxCurr]
-                Mdl.beta[[CompCurr]][[parCurr]] <- betaCurr
+                    betaCurr <- matrix(0, betaLen, 1)
+                    betaCurr[betaIdxCurr] <- betaInput[IdxCurr]
+                    Mdl.beta[[CompCurr]][[parCurr]] <- betaCurr
+                  }
               }
+
           }
 
         out <- Mdl.beta
       }
-
-    if(is.list(betaInput))
+    else ## Convert Mdl.beta -> betaVec
       {
         betaVec <- unlist(Mdl.beta)
-        betaVec <- betaVec[betaVec != 0]
+        betaIdxVec <- unlist(Mdl.betaIdx)
+        betaVec <- betaVec[(betaVec != 0) & (betaIdxVec == TRUE)]
         names(betaVec) <- NULL
-
-
         out <- betaVec
       }
     return(out)
