5ed6a9edc44f9888e8d00891e5cead017fa45ea0
Feng Li <m@feng.li>
2012-11-30 11:13:30 +0100

    Rename functions to avoid naming conflict

---
+++ R/models/parCplCaller.R
@@ -0,0 +1,15 @@
+parCplCaller <- function(parUpdate)
+{
+  ## check which parameter need to update
+  CompNM <- names(parUpdate)
+  for(i in CompNM)
+    {
+      parNM <- names(parUpdate[[i]])
+      parUpdateIdx <- parNM[parUpdate[[i]] == TRUE]
+      for(j in parUpdateIdx)
+        {
+          out <- c(i, j)
+        }
+    }
+  return(out)
+}
---
+++ R/models/parCplSwap.R
@@ -0,0 +1,52 @@
+parCplSwap <- function(betaInput, Mdl.beta = NA, Mdl.betaIdx = NA, parUpdate = NA)
+  {
+    ## Convert Mdl.beta -> betaVec
+    if(class(betaInput)  == "list")
+      {
+        for(CompCurr in names(Mdl.betaIdx))
+          {
+            for(parCurr in names(Mdl.betaIdx[[CompCurr]]))
+              {
+                if(parUpdate[[CompCurr]][[parCurr]] == FALSE)
+                  {
+                    Mdl.beta[[CompCurr]][[parCurr]] <- NULL
+                    Mdl.betaIdx[[CompCurr]][[parCurr]] <- NULL
+                  }
+              }
+          }
+        betaVec <- unlist(Mdl.beta)
+        betaIdxVec <- unlist(Mdl.betaIdx)
+        betaVec <- betaVec[betaIdxVec]
+        names(betaVec) <- NULL
+        out <- betaVec
+      }
+    else ## Convert betaVec -> Mdl.beta
+      {
+        ## Convert the parameter vector into the model structure
+        Idx0 <- NA # the initial parameter index
+        Idx1 <- 0
+        for(CompCurr in names(Mdl.betaIdx))
+          {
+            for(parCurr in names(Mdl.betaIdx[[CompCurr]]))
+              {
+                if(parUpdate[[CompCurr]][[parCurr]] == TRUE)
+                  {
+                    betaIdxCurr <- Mdl.betaIdx[[CompCurr]][[parCurr]]
+                    betaLen <- length(betaIdxCurr)
+                    betaLenNoZero <-sum(betaIdxCurr)
+
+                    Idx0 <- Idx1+1
+                    Idx1 <- Idx0 + betaLenNoZero -1
+                    IdxCurr <-Idx0:Idx1
+
+                    betaCurr <- matrix(0, betaLen, 1)
+                    betaCurr[betaIdxCurr] <- betaInput[IdxCurr]
+                    Mdl.beta[[CompCurr]][[parCurr]] <- betaCurr
+                  }
+              }
+
+          }
+        out <- Mdl.beta
+      }
+    return(out)
+  }
