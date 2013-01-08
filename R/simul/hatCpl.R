178f42db0228920d52f560c75bb0c3fccbd6e5ae
Feng Li <m@feng.li>
2013-01-09 00:29:16 +0100

    Debugging update...

--- R/simul/hatCpl.R
+++ R/simul/hatCpl.R
@@ -1,15 +1,15 @@
 ##' Empirical copula estimation
 ##'
-##' Non-parametric method of estimating copula
-##' @param u "numeric vector"
-##' @param v "numeric vector"
+##' Non-parametric method of estimating copula.
+##' @param u "numeric vector".
+##' @param v "numeric vector".
 ##'
 ##'        Note that u and v should of the same length and both u and v should
 ##'        be in the interval of [0, 1].
 ##'
-##' @param x "numeric"
-##' @param y "numeric"
-##' @return "numeric"
+##' @param x "numeric".
+##' @param y "numeric".
+##' @return "numeric".
 ##'
 ##'         The numeric estimation of copula for data x and y.
 ##'
@@ -21,16 +21,33 @@ hatCpl <- function(u, v, x, y)
   {
 
     n <- length(x)
-    i <- round(u*n)
-    j <- round(v*n)
+
+    out <- u
+    out[0:length(u)] <- 0
+
+    if(length(u) != length(v))
+      {
+        stop("u and v should have the same length")
+      }
+
+
+    iAll <- round(u*n)
+    jAll <- round(v*n)
+
+    IdxNoZero <- (iAll!= 0 & jAll != 0)
+
+    i <- iAll[IdxNoZero]
+    j <- jAll[IdxNoZero]
+
+
+    ## quit if i and j are of length zero
+    if(length(i) == 0)
+      {
+        return(out)
+      }
 
     x.ord <- order(x)
     y.ord <- order(y)
-
-    if(length(i) != length(j))
-      {
-        stop("i and j should have the same length")
-      }
 
     n.ij <- length(i)
 
@@ -45,6 +62,7 @@ hatCpl <- function(u, v, x, y)
 
     sign.mat <- matrix(0, n, n.ij)
     sign.mat[as.vector(x.sign.mat & y.sign.mat)] <- 1
-    out <- colMeans(sign.mat)
+
+    out[IdxNoZero] <- colMeans(sign.mat)
     return(out)
   }
