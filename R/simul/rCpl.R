23c10030731704744b0cbbaa74276328c7ce1525
Feng Li <m@feng.li>
2016-05-16 00:00:42 +0800

    Stablize copula random number function

--- R/simul/rCpl.R
+++ R/simul/rCpl.R
@@ -37,7 +37,7 @@ rCpl <- function(n, parCpl, CplNM, exArgs = NA)
 
         ## The conditional density equation C(u2|u1)  =  v2
         ## Not in vector form
-        logcondEQ <- function(x, delta, theta, v)
+        logcondEQ <- function(x, delta, theta, v, power)
         {
             u1 <- v[1]
             u2 <- x
@@ -46,31 +46,66 @@ rCpl <- function(n, parCpl, CplNM, exArgs = NA)
             L1 <- 1-(1-u1)^theta
             L2 <- 1-(1-u2)^theta
             L5 <- -1 + L1^(-delta) + L2^(-delta)
+
             L6 <- 1- L5^(-1/delta) # FIXME: log(L6)->Inf when u->1,  v->1.
 
             logcondDens <- -(1+delta)*log(L1) - (1+1/delta)*log(L5) +
                 (-1+1/theta)*log(L6) + (-1+theta)*log(1-u1)
 
-            out <- logcondDens - log(v2)
+            out <- (logcondDens - log(v2))^power
+
             return(out)
         }
+
+
+        logcondEQ.stable <- function(x, delta, theta, v, power = 1)
+        {
+            out <- logcondEQ(x = x, delta = delta, theta = theta, v = v,
+                             power = power)
+            if(!is.finite(out) || is.na(out))
+            {
+                require("Rmpfr")
+                precBits <- 1024
+                out.mpfr <- logcondEQ(x = mpfr(x, precBits = precBits),
+                                      delta = mpfr(delta, precBits = precBits),
+                                      theta = mpfr(theta, precBits = precBits),
+                                      v = mpfr(v, precBits = precBits),
+                                      power = power)
+                out <- as.numeric(out.mpfr)
+
+            }
+            return(out)
+        }
+
+
 
         ## Solve u2 from v2 = C(u2|u1) No direct solution, use nonlinear
         ## solver. TODO: Potential overflow when theta is big. Consider using
         ## Chris Sims solver.
+        tol = 1e-10
         for(i in 1:n)
         {
-            out.u2.try <- try(uniroot(f = logcondEQ, interval = c(0, 1),
-                                      theta = theta[i], delta = delta[i], v = v[i,]),
+            ## uniroot() is usually fast but instable for the boundary.
+            u2.uniroot <- try(uniroot(f = logcondEQ.stable,
+                                      interval = c(0+tol, 1-tol),
+                                      theta = theta[i], delta = delta[i],
+                                      v = v[i,], power = 1),
                               silent = TRUE)
 
-            if(is(out.u2.try, "try-error"))
+            if(is(u2.uniroot, "try-error"))
             {
-                out.u2 <- NA
+                u2.optim <- optim(par = 0.5, logcondEQ.stable,
+                                  lower = 0.001, upper = 0.999,
+                                  theta = theta[i], delta = delta[i],
+                                  v = v[i,], power = 2,
+                                  method = "L-BFGS-B")
+
+                out.u2 <- u2.optim$par
+                ## warning("NA produced in generating BB7 copula random numbers.")
             }
             else
             {
-                out.u2 <- out.u2.try[["root"]]
+                out.u2 <- u2.uniroot[["root"]]
             }
 
             u[i, 2] <- out.u2
