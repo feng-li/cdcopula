3233a7a5c176345bec2167335891ba1a103603cc
Feng Li <m@feng.li>
2016-06-04 18:35:46 +0800

    Add slurm file for batch jobs

--- inst/config/config.MVT2.SPLITT.SP100-SP600.R
+++ inst/config/config.MVT2.SPLITT.SP100-SP600.R
@@ -88,8 +88,8 @@ Mdl.X[[2]][["phi"]] <- cbind(1, X[[2]][Mdl.dataUsedIdx, 1:9])
 Mdl.X[[2]][["df"]] <- cbind(1, X[[2]][Mdl.dataUsedIdx, 1:9])
 Mdl.X[[2]][["lmd"]] <- cbind(1, X[[2]][Mdl.dataUsedIdx, 1:9])
 
-Mdl.X[[3]][["df"]] <- cbind(1, X[[1]][Mdl.dataUsedIdx, 1:9], X[[2]][Mdl.dataUsedIdx, 1:9])
-Mdl.X[[3]][["rho"]] <- cbind(1, X[[1]][Mdl.dataUsedIdx, 1:9], X[[2]][Mdl.dataUsedIdx, 1:9])
+Mdl.X[[3]][["df"]] <- cbind(1, X[[1]][Mdl.dataUsedIdx, 1:9], X[[2]][Mdl.dataUsedIdx, 1:9])[, 1, drop = FALSE]
+Mdl.X[[3]][["rho"]] <- cbind(1, X[[1]][Mdl.dataUsedIdx, 1:9], X[[2]][Mdl.dataUsedIdx, 1:9])[, 1, drop = FALSE]
 
 ## THE LINK FUNCTION USED IN THE MODEL
 Mdl.parLink <- MCMC.Update
@@ -169,7 +169,7 @@ MCMC.UpdateOrder[[3]][[2]] <- 10
 ## density. A variable "MCMC.density[["u"]]" must provide. "MCMC.density" consists of CDF
 ## of margins (i.e. u1, u2, ...)
 
-MCMC.UpdateStrategy <- "twostage"
+MCMC.UpdateStrategy <- "joint"
 
 ## THE METROPOLIS-HASTINGS ALGORITHM PROPOSAL ARGUMENTS
 MCMC.propArgs <- MCMC.Update
