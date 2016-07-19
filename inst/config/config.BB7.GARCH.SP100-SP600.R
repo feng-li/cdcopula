5c3a122c8af12c4090e767dab2a4b9e8164615e4
Feng Li <m@feng.li>
2016-07-19 15:17:54 +0800

    Minor update

--- inst/config/config.BB7.GARCH.SP100-SP600.R
+++ inst/config/config.BB7.GARCH.SP100-SP600.R
@@ -3,23 +3,23 @@
 ###        COPULA MODEL WITH COVARIATE--DEPENDENT
 ###
 ### SYSTEM REQUIREMENTS
-###        R > = 2.15.3 with packages ``mvtnorm'',  ``parallel''
-###        BLAS (optional)
+###        R > = 2.15.3 WITH PACKAGES ``MVTNORM'',  ``PARALLEL''
+###        BLAS (OPTIONAL)
 ###
 ### INPUT  VARIABLES
-###        The variables with comments in capital letters are user input
-###        variables.
+###        THE VARIABLES WITH COMMENTS IN CAPITAL LETTERS ARE USER INPUT
+###        VARIABLES.
 ###
 ### OUTPUT VARIABLES
-###        The output variables are always started with ``MCMC.''
+###        THE OUTPUT VARIABLES ARE ALWAYS STARTED WITH ``MCMC.''
 ###
 ### GENERALIZATION
-###        If you are interested in developing new copula models based on the
-###        existing code. Look into the ``model'' folder.
+###        IF YOU ARE INTERESTED IN DEVELOPING NEW COPULA MODELS BASED ON THE
+###        EXISTING CODE. LOOK INTO THE ``MODEL'' FOLDER.
 ###
 ### DATE
-###        CREATED: Mon Jan 09 17:12:04 CET 2012
-###        CURRENT: Sun Jan 04 10:21:56 CST 2015
+###        CREATED: MON JAN 09 17:12:04 CET 2012
+###        CURRENT: SUN JAN 04 10:21:56 CST 2015
 ###############################################################################
 
 
@@ -27,21 +27,21 @@
 ### SPECIFY THE MODEL
 ###----------------------------------------------------------------------------
 ## MARGINAL MODELS NAME, TYPE AND PARAMETERS
-Mdl.MargisType <- c("GARCH", "GARCH", "BB7")
-Mdl.MargisNM <- c("^SML", "^OEX", "BB7")
+MDL.MARGISTYPE <- C("GARCH", "GARCH", "BB7")
+MDL.MARGISNM <- C("^SML", "^OEX", "BB7")
 
-MCMC.Update <- list(list("mu" = F, "phi" = F),
-                    list("mu" = F, "phi" = F),
-                    list("lambdaL" = T, "tau" = T))
+MCMC.UPDATE <- LIST(LIST("MU" = F, "PHI" = F),
+                    LIST("MU" = F, "PHI" = F),
+                    LIST("LAMBDAL" = T, "TAU" = T))
 
-names(MCMC.Update) <- Mdl.MargisNM
+NAMES(MCMC.UPDATE) <- MDL.MARGISNM
 
 ## THE MODEL EVALUATION CRITERION
-## Set this to NULL to turn of evaluation.
-LPDS <- c("joint", Mdl.MargisNM)
+## SET THIS TO NULL TO TURN OF EVALUATION.
+LPDS <- C("JOINT", MDL.MARGISNM)
 
-## The object structure for the model components
-names(Mdl.MargisType) <-  Mdl.MargisNM
+## THE OBJECT STRUCTURE FOR THE MODEL COMPONENTS
+NAMES(MDL.MARGISTYPE) <-  MDL.MARGISNM
 
 ###----------------------------------------------------------------------------
 ### THE DATA AND MODEL
@@ -49,144 +49,144 @@ names(Mdl.MargisType) <-  Mdl.MargisNM
 
 ## THE DATASET
 ##-----------------------------------------------------------------------------
-## The dataset should either provided via DGP or the real dataset. The dataset
-## should be in the following structure:
-## Mdl.X: "list" each list contains the covariates in each margin or copula.
-## Mdl.Y: "list" each list contains the response variable of that margin.
+## THE DATASET SHOULD EITHER PROVIDED VIA DGP OR THE REAL DATASET. THE DATASET
+## SHOULD BE IN THE FOLLOWING STRUCTURE:
+## MDL.X: "LIST" EACH LIST CONTAINS THE COVARIATES IN EACH MARGIN OR COPULA.
+## MDL.Y: "LIST" EACH LIST CONTAINS THE RESPONSE VARIABLE OF THAT MARGIN.
 
-load(file.path(R_CPL_LIB_ROOT_DIR, "data/SP100-SP400-SP600-20150206.Rdata"))
+LOAD(FILE.PATH(R_CPL_LIB_ROOT_DIR, "DATA/SP100-SP400-SP600-20150206.RDATA"))
 
-## No. of Total Observations
-nObsRaw <- length(Y[[1]])
+## NO. OF TOTAL OBSERVATIONS
+NOBSRAW <- LENGTH(Y[[1]])
 
-## Data subset used
-Mdl.dataUsedIdx <- (1 + nObsRaw-nObsRaw):nObsRaw
+## DATA SUBSET USED
+MDL.DATAUSEDIDX <- (1 + NOBSRAW-NOBSRAW):NOBSRAW
 
 
 ## THE RESPONSE VARIABLES
-Mdl.Y <- lapply(Y[Mdl.MargisNM[-length(Mdl.MargisNM)]], function(x, idx)x[idx, ,drop = FALSE], Mdl.dataUsedIdx)
+MDL.Y <- LAPPLY(Y[MDL.MARGISNM[-LENGTH(MDL.MARGISNM)]], FUNCTION(X, IDX)X[IDX, ,DROP = FALSE], MDL.DATAUSEDIDX)
 
-## The name of respond variables
-names(Mdl.Y) <- Mdl.MargisNM[-length(Mdl.MargisNM)]
+## THE NAME OF RESPOND VARIABLES
+NAMES(MDL.Y) <- MDL.MARGISNM[-LENGTH(MDL.MARGISNM)]
 
 ## COVARIATES USED FOR THE MARGINAL AND COPULA PARAMETERS
 ## ------------------------------------------------------------------------------
 
-## A trick to include foreign marginal models in the estimation which are hard to directly
-## put into the "MargiModel()" is do the following settings: (1) Let "MCMC.Update" be FALSE
-## in all marginal densities.  (2) Estimate the density features in foreign models and set
-## the features in "Mdl.X" directly.  (3) Set MCMC.UpdateStrategy be "twostage". (4) Set
-## "Mdl.betaInit" be one in all marginal features.
-Mdl.X <- MCMC.Update
+## A TRICK TO INCLUDE FOREIGN MARGINAL MODELS IN THE ESTIMATION WHICH ARE HARD TO DIRECTLY
+## PUT INTO THE "MARGIMODEL()" IS DO THE FOLLOWING SETTINGS: (1) LET "MCMC.UPDATE" BE FALSE
+## IN ALL MARGINAL DENSITIES.  (2) ESTIMATE THE DENSITY FEATURES IN FOREIGN MODELS AND SET
+## THE FEATURES IN "MDL.X" DIRECTLY.  (3) SET MCMC.UPDATESTRATEGY BE "TWOSTAGE". (4) SET
+## "MDL.BETAINIT" BE ONE IN ALL MARGINAL FEATURES.
+MDL.X <- MCMC.UPDATE
 
-Mdl.X[[1]] <- list(include.mean = TRUE, cond.dist = "norm", trace = TRUE)
-Mdl.X[[2]] <- list(include.mean = TRUE, cond.dist = "norm", trace = TRUE)
+MDL.X[[1]] <- LIST(INCLUDE.MEAN = TRUE, COND.DIST = "NORM", TRACE = TRUE)
+MDL.X[[2]] <- LIST(INCLUDE.MEAN = TRUE, COND.DIST = "NORM", TRACE = TRUE)
 
-Mdl.X[[3]][["lambdaL"]] <- cbind(1, X[[1]][Mdl.dataUsedIdx, 1:9], X[[2]][Mdl.dataUsedIdx, 1:9])
-Mdl.X[[3]][["tau"]] <- cbind(1, X[[1]][Mdl.dataUsedIdx, 1:9], X[[2]][Mdl.dataUsedIdx, 1:9])
+MDL.X[[3]][["LAMBDAL"]] <- CBIND(1, X[[1]][MDL.DATAUSEDIDX, 1:9], X[[2]][MDL.DATAUSEDIDX, 1:9])
+MDL.X[[3]][["TAU"]] <- CBIND(1, X[[1]][MDL.DATAUSEDIDX, 1:9], X[[2]][MDL.DATAUSEDIDX, 1:9])
 
 ## THE LINK FUNCTION USED IN THE MODEL
-Mdl.parLink <- MCMC.Update
-Mdl.parLink[[1]][["mu"]] <- list(type = "identity", nPar = 1)
-Mdl.parLink[[1]][["phi"]] <- list(type = "identity", nPar = 1)
+MDL.PARLINK <- MCMC.UPDATE
+MDL.PARLINK[[1]][["MU"]] <- LIST(TYPE = "IDENTITY", NPAR = 1)
+MDL.PARLINK[[1]][["PHI"]] <- LIST(TYPE = "IDENTITY", NPAR = 1)
 
-Mdl.parLink[[2]][["mu"]] <- list(type = "identity", nPar = 1)
-Mdl.parLink[[2]][["phi"]] <- list(type = "identity", nPar = 1)
+MDL.PARLINK[[2]][["MU"]] <- LIST(TYPE = "IDENTITY", NPAR = 1)
+MDL.PARLINK[[2]][["PHI"]] <- LIST(TYPE = "IDENTITY", NPAR = 1)
 
-Mdl.parLink[[3]][["lambdaL"]] <- list(type = "glogit", nPar = 1, a = 0.01, b = 0.99)
-Mdl.parLink[[3]][["tau"]] <- list(type = "glogit", nPar = 1, a = 0.01, b = 0.99)
+MDL.PARLINK[[3]][["LAMBDAL"]] <- LIST(TYPE = "GLOGIT", NPAR = 1, A = 0.01, B = 0.99)
+MDL.PARLINK[[3]][["TAU"]] <- LIST(TYPE = "GLOGIT", NPAR = 1, A = 0.01, B = 0.99)
 
 ## THE VARIABLE SELECTION SETTINGS AND STARTING POINT
-## Variable selection candidates, NULL: no variable selection use full
-## covariates. ("all-in", "all-out", "random", or user-input)
+## VARIABLE SELECTION CANDIDATES, NULL: NO VARIABLE SELECTION USE FULL
+## COVARIATES. ("ALL-IN", "ALL-OUT", "RANDOM", OR USER-INPUT)
 
-Mdl.varSelArgs <- MCMC.Update
-Mdl.varSelArgs[[1]][["mu"]] <- list(cand = NULL, init = "all-in")
-Mdl.varSelArgs[[1]][["phi"]] <- list(cand = NULL, init = "all-in")
+MDL.VARSELARGS <- MCMC.UPDATE
+MDL.VARSELARGS[[1]][["MU"]] <- LIST(CAND = NULL, INIT = "ALL-IN")
+MDL.VARSELARGS[[1]][["PHI"]] <- LIST(CAND = NULL, INIT = "ALL-IN")
 
-Mdl.varSelArgs[[2]][["mu"]] <- list(cand = NULL, init = "all-in")
-Mdl.varSelArgs[[2]][["phi"]] <- list(cand = NULL, init = "all-in")
+MDL.VARSELARGS[[2]][["MU"]] <- LIST(CAND = NULL, INIT = "ALL-IN")
+MDL.VARSELARGS[[2]][["PHI"]] <- LIST(CAND = NULL, INIT = "ALL-IN")
 
-Mdl.varSelArgs[[3]][["lambdaL"]] <- list(cand = "2:end", init = "all-in")
-Mdl.varSelArgs[[3]][["tau"]] <- list(cand = "2:end", init = "all-in")
+MDL.VARSELARGS[[3]][["LAMBDAL"]] <- LIST(CAND = "2:END", INIT = "ALL-IN")
+MDL.VARSELARGS[[3]][["TAU"]] <- LIST(CAND = "2:END", INIT = "ALL-IN")
 
 ###----------------------------------------------------------------------------
 ### THE MCMC CONFIGURATION
 ###----------------------------------------------------------------------------
 
 ## NUMBER OF MCMC ITERATIONS
-MCMC.nIter <- 1000
+MCMC.NITER <- 1000
 
 ## SAVE OUTPUT PATH
 ##-----------------------------------------------------------------------------
-## "save.output = FALSE" it will not save anything.
-## "save.output = "path-to-directory"" it will save the working directory in
-## the given directory.
-save.output <- "~/running"
+## "SAVE.OUTPUT = FALSE" IT WILL NOT SAVE ANYTHING.
+## "SAVE.OUTPUT = "PATH-TO-DIRECTORY"" IT WILL SAVE THE WORKING DIRECTORY IN
+## THE GIVEN DIRECTORY.
+SAVE.OUTPUT <- "~/RUNNING"
 
 ## MCMC TRAJECTORY
 ##-----------------------------------------------------------------------------
-## If TRUE,  the MCMC should be tracked during the evaluation.
-MCMC.track <- TRUE
+## IF TRUE,  THE MCMC SHOULD BE TRACKED DURING THE EVALUATION.
+MCMC.TRACK <- TRUE
 
-MCMC.UpdateOrder <- MCMC.Update
-MCMC.UpdateOrder[[1]][[1]] <- 1
+MCMC.UPDATEORDER <- MCMC.UPDATE
+MCMC.UPDATEORDER[[1]][[1]] <- 1
 
-MCMC.UpdateOrder[[2]][[1]] <- 2
+MCMC.UPDATEORDER[[2]][[1]] <- 2
 
-MCMC.UpdateOrder[[3]][[1]] <- 3
-MCMC.UpdateOrder[[3]][[2]] <- 4
+MCMC.UPDATEORDER[[3]][[1]] <- 3
+MCMC.UPDATEORDER[[3]][[2]] <- 4
 
 ## MCMC UPDATING STRATEGY
 ##-----------------------------------------------------------------------------
-## "joint"    : Update the joint posterior w.r.t. MCMC.Update and MCMC.UpdateOrder
-## "margin"   : the marginal posterior.
-## "twostage" : Update the joint posterior but using a two stage approach.
+## "JOINT"    : UPDATE THE JOINT POSTERIOR W.R.T. MCMC.UPDATE AND MCMC.UPDATEORDER
+## "MARGIN"   : THE MARGINAL POSTERIOR.
+## "TWOSTAGE" : UPDATE THE JOINT POSTERIOR BUT USING A TWO STAGE APPROACH.
 
-## NOTE: If one want to use "margin" or "twostage" options just to to estimate the copula
-## density. A variable "MCMC.density[["u"]]" must provide. "MCMC.density" consists of CDF of
-## margins (i.e. u1,  u2, ...)
+## NOTE: IF ONE WANT TO USE "MARGIN" OR "TWOSTAGE" OPTIONS JUST TO TO ESTIMATE THE COPULA
+## DENSITY. A VARIABLE "MCMC.DENSITY[["U"]]" MUST PROVIDE. "MCMC.DENSITY" CONSISTS OF CDF OF
+## MARGINS (I.E. U1,  U2, ...)
 
-MCMC.UpdateStrategy <- "twostage"
+MCMC.UPDATESTRATEGY <- "TWOSTAGE"
 
 ## THE METROPOLIS-HASTINGS ALGORITHM PROPOSAL ARGUMENTS
-MCMC.propArgs <- MCMC.Update
-MCMC.propArgs[[1]][[1]] <- NA
-MCMC.propArgs[[1]][[2]] <- NA
+MCMC.PROPARGS <- MCMC.UPDATE
+MCMC.PROPARGS[[1]][[1]] <- NA
+MCMC.PROPARGS[[1]][[2]] <- NA
 
-MCMC.propArgs[[2]][[1]] <- NA
-MCMC.propArgs[[2]][[2]] <- NA
+MCMC.PROPARGS[[2]][[1]] <- NA
+MCMC.PROPARGS[[2]][[2]] <- NA
 
-MCMC.propArgs[[3]][[1]] <- list("algorithm" = list(type = "GNewtonMove", ksteps = 3, hess = "outer"),
-                           "beta" = list(type = "mvt", df = 6),
-                           "indicators" = list(type = "binom", prob = 0.5))
-MCMC.propArgs[[3]][[2]] <- list("algorithm" = list(type = "GNewtonMove", ksteps = 3, hess = "outer"),
-                           "beta" = list(type = "mvt", df = 6),
-                           "indicators" = list(type = "binom", prob = 0.5))
+MCMC.PROPARGS[[3]][[1]] <- LIST("ALGORITHM" = LIST(TYPE = "GNEWTONMOVE", KSTEPS = 3, HESS = "OUTER"),
+                           "BETA" = LIST(TYPE = "MVT", DF = 6),
+                           "INDICATORS" = LIST(TYPE = "BINOM", PROB = 0.5))
+MCMC.PROPARGS[[3]][[2]] <- LIST("ALGORITHM" = LIST(TYPE = "GNEWTONMOVE", KSTEPS = 3, HESS = "OUTER"),
+                           "BETA" = LIST(TYPE = "MVT", DF = 6),
+                           "INDICATORS" = LIST(TYPE = "BINOM", PROB = 0.5))
 
 ## POSTERIOR INFERENCE OPTIONS
 ##-----------------------------------------------------------------------------
 
 ## CROSS VALIDATION
-## "N.subsets" is no. of folds for cross-validation. If N.subsets  =  0, no
-## cross-validation. And "partiMethod" tells how to partition the data. Testing
-## percent is used if partiMethod is "time-series". (use the old data to
-## predict the new interval)
+## "N.SUBSETS" IS NO. OF FOLDS FOR CROSS-VALIDATION. IF N.SUBSETS  =  0, NO
+## CROSS-VALIDATION. AND "PARTIMETHOD" TELLS HOW TO PARTITION THE DATA. TESTING
+## PERCENT IS USED IF PARTIMETHOD IS "TIME-SERIES". (USE THE OLD DATA TO
+## PREDICT THE NEW INTERVAL)
 
-nCross <- 1
-Mdl.crossValidArgs <- list(N.subsets = nCross,
-                       partiMethod = "time-series",
-                       testRatio = 0.2)
+NCROSS <- 1
+MDL.CROSSVALIDARGS <- LIST(N.SUBSETS = NCROSS,
+                       PARTIMETHOD = "TIME-SERIES",
+                       TESTRATIO = 0.2)
 
-## Indices for training and testing sample according to cross-validation
-Mdl.crossValidIdx <- set.crossvalid(length(Mdl.dataUsedIdx),Mdl.crossValidArgs)
-## nCrossFold <- length(Mdl.crossValidIdx[["training"]])
+## INDICES FOR TRAINING AND TESTING SAMPLE ACCORDING TO CROSS-VALIDATION
+MDL.CROSSVALIDIDX <- SET.CROSSVALID(LENGTH(MDL.DATAUSEDIDX),MDL.CROSSVALIDARGS)
+## NCROSSFOLD <- LENGTH(MDL.CROSSVALIDIDX[["TRAINING"]])
 
 ## SAMPLER PROPORTION FOR POSTERIOR INFERENCE,
-MCMC.sampleProp <- 1
+MCMC.SAMPLEPROP <- 1
 
 ## BURN-IN RATIO
-MCMC.burninProp <- 0.1 # zero indicates no burn-in
+MCMC.BURNINPROP <- 0.1 # ZERO INDICATES NO BURN-IN
 
 ###----------------------------------------------------------------------------
 ### PRIOR SETTINGS
@@ -194,54 +194,54 @@ MCMC.burninProp <- 0.1 # zero indicates no burn-in
 
 ## PRIOR FOR THE COPULA PARAMETERS
 ## -----------------------------------------------------------------------------
-## NOTE: The variable are recycled if needed. For example indicators$prob can be a scaler
-## or a vector with same length of variable section candidates. There might be connections
-## between parameters in the models but is will not affect the prior settings on the
-## coefficients as long as we use a dynamic link function.
+## NOTE: THE VARIABLE ARE RECYCLED IF NEEDED. FOR EXAMPLE INDICATORS$PROB CAN BE A SCALER
+## OR A VECTOR WITH SAME LENGTH OF VARIABLE SECTION CANDIDATES. THERE MIGHT BE CONNECTIONS
+## BETWEEN PARAMETERS IN THE MODELS BUT IS WILL NOT AFFECT THE PRIOR SETTINGS ON THE
+## COEFFICIENTS AS LONG AS WE USE A DYNAMIC LINK FUNCTION.
 
-Mdl.priArgs <- MCMC.Update
+MDL.PRIARGS <- MCMC.UPDATE
 
-Mdl.priArgs[[1]][["mu"]] <- NA
-Mdl.priArgs[[1]][["phi"]] <- NA
+MDL.PRIARGS[[1]][["MU"]] <- NA
+MDL.PRIARGS[[1]][["PHI"]] <- NA
 
-Mdl.priArgs[[2]][["mu"]] <- NA
-Mdl.priArgs[[2]][["phi"]] <- NA
+MDL.PRIARGS[[2]][["MU"]] <- NA
+MDL.PRIARGS[[2]][["PHI"]] <- NA
 
 
-Mdl.priArgs[[3]][["lambdaL"]] <- list("beta" = list("intercept" = list(type = "custom",
-                                                                   input = list(type = "gbeta",  mean = 0.2, variance = 0.05,
-                                                                                a = 0.01, b = 0.99),
-                                                                   output = list(type = "norm", shrinkage = 1)),
-                                                "slopes" = list(type = "cond-mvnorm",
-                                                                mean = 0, covariance = "identity", shrinkage = 1)),
-                                  "indicators" = list(type = "bern", prob = 0.5))
-Mdl.priArgs[[3]][["tau"]] <- list("beta" = list("intercept" = list(type = "custom",
-                                                                   input = list(type = "gbeta",  mean = 0.2, variance = 0.05,
-                                                                                a = 0.01, b = 0.99),
-                                                                   output = list(type = "norm", shrinkage = 1)),
-                                                "slopes" = list(type = "cond-mvnorm",
-                                                                mean = 0, covariance = "identity", shrinkage = 1)),
-                                  "indicators" = list(type = "bern", prob = 0.5))
+MDL.PRIARGS[[3]][["LAMBDAL"]] <- LIST("BETA" = LIST("INTERCEPT" = LIST(TYPE = "CUSTOM",
+                                                                   INPUT = LIST(TYPE = "GBETA",  MEAN = 0.2, VARIANCE = 0.05,
+                                                                                A = 0.01, B = 0.99),
+                                                                   OUTPUT = LIST(TYPE = "NORM", SHRINKAGE = 1)),
+                                                "SLOPES" = LIST(TYPE = "COND-MVNORM",
+                                                                MEAN = 0, COVARIANCE = "IDENTITY", SHRINKAGE = 1)),
+                                  "INDICATORS" = LIST(TYPE = "BERN", PROB = 0.5))
+MDL.PRIARGS[[3]][["TAU"]] <- LIST("BETA" = LIST("INTERCEPT" = LIST(TYPE = "CUSTOM",
+                                                                   INPUT = LIST(TYPE = "GBETA",  MEAN = 0.2, VARIANCE = 0.05,
+                                                                                A = 0.01, B = 0.99),
+                                                                   OUTPUT = LIST(TYPE = "NORM", SHRINKAGE = 1)),
+                                                "SLOPES" = LIST(TYPE = "COND-MVNORM",
+                                                                MEAN = 0, COVARIANCE = "IDENTITY", SHRINKAGE = 1)),
+                                  "INDICATORS" = LIST(TYPE = "BERN", PROB = 0.5))
 
 ###----------------------------------------------------------------------------
 ### THE PARAMETERS FOR INITIAL AND CURRENT MCMC ITERATION
-### The parameters in the current MCMC iteration. For the first iteration, it
-### is set as the initial values
+### THE PARAMETERS IN THE CURRENT MCMC ITERATION. FOR THE FIRST ITERATION, IT
+### IS SET AS THE INITIAL VALUES
 ###----------------------------------------------------------------------------
 
 ## THE PARAMETER COEFFICIENTS STARTING POINT
-## The possible inputs are ("random", "ols"  or user-input).
-Mdl.betaInit <- MCMC.Update
-Mdl.betaInit[[1]][[1]] <- 1
-Mdl.betaInit[[1]][[2]] <- 1
+## THE POSSIBLE INPUTS ARE ("RANDOM", "OLS"  OR USER-INPUT).
+MDL.BETAINIT <- MCMC.UPDATE
+MDL.BETAINIT[[1]][[1]] <- 1
+MDL.BETAINIT[[1]][[2]] <- 1
 
-Mdl.betaInit[[2]][[1]] <- 1
-Mdl.betaInit[[2]][[2]] <- 1
+MDL.BETAINIT[[2]][[1]] <- 1
+MDL.BETAINIT[[2]][[2]] <- 1
 
-Mdl.betaInit[[3]][[1]] <- "random"
-Mdl.betaInit[[3]][[2]] <- "random"
+MDL.BETAINIT[[3]][[1]] <- "RANDOM"
+MDL.BETAINIT[[3]][[2]] <- "RANDOM"
 
-MCMC.optimInit <- TRUE
+MCMC.OPTIMINIT <- TRUE
 ################################################################################
 ###                                  THE END
 ################################################################################
